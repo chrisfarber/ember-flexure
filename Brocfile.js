ef03423fefadb82601086524e9a77f416d47425f
Chris Farber <chris@chrisfarber.net>
2014-10-27 19:26:59 -0400

    Basic brocfile

---
+++ Brocfile.js
@@ -0,0 +1,22 @@
+var pickFiles     = require("broccoli-static-compiler");
+var coffee        = require("broccoli-coffee");
+var es6           = require("broccoli-es6-module-transpiler");
+var merge         = require('broccoli-merge-trees');
+var moveFile      = require('broccoli-file-mover');
+
+var source = pickFiles("src", {
+  srcDir: "/",
+  files: ["**/*.coffee"],
+  destDir: "/flexure"
+});
+
+source = coffee(source, {
+  bare: true,
+});
+
+source = es6(source, {
+  formatter: "bundle",
+  output: "ember-flexure.js"
+});
+
+module.exports = source;
