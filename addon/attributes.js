27f8b2a254dd6bb5567caef509ccbd3342b86b27
Chris Farber <chris@chrisfarber.net>
2015-07-02 14:52:52 -0400

    Fix bug

--- addon/attributes.js
+++ addon/attributes.js
@@ -43,7 +43,7 @@ export function hasMany(model, meta = {}) {
 
   let getStorage = (m, k) => {
     return m._rawRead(k, () => {
-      return m.models.makeRelationshipArray(m, []);
+      return m.models.makeRelationshipArray(model, []);
     });
   };
 
